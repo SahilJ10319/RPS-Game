2bc719a4719863b094dc43f36cf59420275635b9
Sahil Janjua <sahil_janjua@sfu.ca>
2023-02-06 01:10:36 -0800

    Added User Choice Function

--- script.js
+++ script.js
@@ -4,10 +4,29 @@ let userChoice = '';
 let userScore = 0;
 let computerChoice = '';
 let computerScore = 0;
+let tries = 0;
 
 function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * choices.length);
     return choices[computerChoice];
 }
 
-console.log(getComputerChoice());
+function getUserChoice() {
+    while (tries < 3) { 
+        userChoice = prompt("rock, paper, or scissors?").toLowerCase();
+        switch(userChoice) {
+            case "rock":
+            case "scissors":
+            case "paper":
+                console.log(`You chose ${userChoice}`);
+                return userChoice;
+            default:
+                console.log("Invalid Choice! Try Again");
+                tries++;
+        }
+    }
+    console.log("Maximum Number of Tries Reached! Game Over!");
+}
+
+// console.log(getComputerChoice());
+console.log(getUserChoice());
