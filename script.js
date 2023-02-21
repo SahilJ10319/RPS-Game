d9a1ee0372d508a3a8b208478f2b5fc131591500
Sahil Janjua <sahil_janjua@sfu.ca>
2023-02-20 23:13:59 -0800

    Finished Game implementation based on Odin Project Requirements, May make changes later

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const ROCK = 'rock';
 const SCISSORS = 'scissors';
 const PAPER = 'paper';
 const choices = [ROCK, PAPER, SCISSORS];
+const rounds = 5;
 
 let userChoice = '';
-let userScore = 0;
 let computerChoice = '';
-let computerScore = 0;
 let tries = 0;
 let result = '';
+let userWins = 0
+let computerWins = 0
 
 function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * choices.length);
@@ -35,41 +36,66 @@ function getUserChoice() {
 function playRound(userChoice, computerChoice) {
     userChoice = getUserChoice();
     computerChoice = getComputerChoice();
-    result = ` ${userChoice} vs ${computerChoice} `;
+    result = `${userChoice} vs ${computerChoice}`;
     
     if (userChoice !== ROCK && userChoice !== SCISSORS && userChoice !== PAPER) {
-        return result + "Invalid choice. Please choose ROCK, SCISSORS, or PAPER.";
+        return console.log(result + "Invalid choice. Please choose ROCK, SCISSORS, or PAPER.");
     }
     
     if (userChoice === computerChoice) {
-        return result + ", It's a tie";
+        return console.log(result + ", It's a tie");
     }
     
     switch(userChoice) {
         case ROCK:
             if(computerChoice == PAPER) {
-                return result + ", You lose! Paper beats Rock!";
+                computerWins++;
+                return console.log(result + ", You lose! Paper beats Rock!");
             }
             else if(computerChoice == SCISSORS) {
-                return result + ", You Win! Rock beats Scissors!";
+                userWins++;
+                return console.log(result + ", You Win! Rock beats Scissors!");
             }
             break;
         case SCISSORS:
             if(computerChoice == PAPER) {
-                return result + ", You Win! Scissors beats Paper!";
+                userWins++;
+                return console.log(result + ", You Win! Scissors beats Paper!");
             }
             else if(computerChoice == ROCK) {
-                return result + ", You lose! Rock beats Scissors!";
+                computerWins++;
+                return console.log(result + ", You lose! Rock beats Scissors!");
             }
             break;
         case PAPER:
             if(computerChoice == ROCK) {
-                return result + ", You Win! Paper beats Rock!";
+                userWins++;
+                return console.log(result + ", You Win! Paper beats Rock!");
             }
             else if(computerChoice == SCISSORS) {
-                return result + ", You lose! Scissors beats Paper!";
+                computerWins++;
+                return console.log(result + ", You lose! Scissors beats Paper!");
             }
             break;
     }
 }
 
+function game() {
+    console.log("Welcome to Rock, Paper, Scissors. There will be 5 games, the one with the most number wins per round wins the game, a tie does not award a point")
+    for(let i = 0; i < rounds; i++) {
+        console.log(`Round ${i + 1}`);
+        playRound();
+        console.log(`User Score: ${userWins} Computer Score: ${computerWins}`);
+    }
+    if(userWins > computerWins) {
+        return console.log("You Win!");
+    }
+    else if(userWins < computerWins) {
+        return console.log("You Lose!");
+    }
+    else {
+        return console.log("It's a tie!");
+    }
+}
+
+game();
