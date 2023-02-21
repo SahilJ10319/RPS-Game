e9235e7c018ef118b10cf3f9a641d7c581e7aa8e
Sahil Janjua <sahil_janjua@sfu.ca>
2023-02-20 21:46:09 -0800

    Added play round function, and added local, and global variables,  and made a few minor variable name changes

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const choices = ["rock", "paper", "scissors"];
+const ROCK = 'rock';
+const SCISSORS = 'scissors';
+const PAPER = 'paper';
+const choices = [ROCK, PAPER, SCISSORS];
 
 let userChoice = '';
 let userScore = 0;
 let computerChoice = '';
 let computerScore = 0;
 let tries = 0;
+let result = '';
 
 function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * choices.length);
@@ -15,9 +19,9 @@ function getUserChoice() {
     while (tries < 3) { 
         userChoice = prompt("rock, paper, or scissors?").toLowerCase();
         switch(userChoice) {
-            case "rock":
-            case "scissors":
-            case "paper":
+            case ROCK:
+            case SCISSORS:
+            case PAPER:
                 console.log(`You chose ${userChoice}`);
                 return userChoice;
             default:
@@ -28,5 +32,44 @@ function getUserChoice() {
     console.log("Maximum Number of Tries Reached! Game Over!");
 }
 
-// console.log(getComputerChoice());
-console.log(getUserChoice());
+function playRound(userChoice, computerChoice) {
+    userChoice = getUserChoice();
+    computerChoice = getComputerChoice();
+    result = ` ${userChoice} vs ${computerChoice} `;
+    
+    if (userChoice !== ROCK && userChoice !== SCISSORS && userChoice !== PAPER) {
+        return result + "Invalid choice. Please choose ROCK, SCISSORS, or PAPER.";
+    }
+    
+    if (userChoice === computerChoice) {
+        return result + ", It's a tie";
+    }
+    
+    switch(userChoice) {
+        case ROCK:
+            if(computerChoice == PAPER) {
+                return result + ", You lose! Paper beats Rock!";
+            }
+            else if(computerChoice == SCISSORS) {
+                return result + ", You Win! Rock beats Scissors!";
+            }
+            break;
+        case SCISSORS:
+            if(computerChoice == PAPER) {
+                return result + ", You Win! Scissors beats Paper!";
+            }
+            else if(computerChoice == ROCK) {
+                return result + ", You lose! Rock beats Scissors!";
+            }
+            break;
+        case PAPER:
+            if(computerChoice == ROCK) {
+                return result + ", You Win! Paper beats Rock!";
+            }
+            else if(computerChoice == SCISSORS) {
+                return result + ", You lose! Scissors beats Paper!";
+            }
+            break;
+    }
+}
+
